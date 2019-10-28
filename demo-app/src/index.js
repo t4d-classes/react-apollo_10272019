766776e8ed4566a152e27324bfb4fa595e1f4ab1
Eric Greene <eric@t4d.io>
2019-10-28 11:35:46 -0700

    add code demos

---
+++ demo-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { HelloWorld } from './components/HelloWorld';
+
+ReactDOM.render(
+  <HelloWorld />,
+  document.querySelector('#root'),
+);
+
+
+
