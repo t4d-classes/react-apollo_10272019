94026c27d7bdc84246e12ae9506e80f16113a72a
Eric Greene <eric@t4d.io>
2019-10-28 13:22:34 -0700

    add code demos

---
+++ demo-app/src/components/ColorTool.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const ColorTool = () => {
+
+  return <>
+    <header>
+      <h1>Color Tool</h1>
+    </header>
+    <ul>
+      <li>black</li>
+      <li>blue</li>
+      <li>orange</li>
+      <li>white</li>
+      <li>green</li>
+      <li>red</li>
+    </ul>
+  </>;
+
+};
