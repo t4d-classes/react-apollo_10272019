309d18d32981947e8583f14a6dfcb05503a3cdfa
Eric Greene <eric@t4d.io>
2019-10-31 14:05:17 -0700

    add code demos

--- apollo-graphql-app-car-tool/src/App.js
+++ apollo-graphql-app-car-tool/src/App.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useQuery } from 'react-apollo';
+import { useQuery, Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const APP_QUERY = gql`
@@ -8,18 +8,27 @@ const APP_QUERY = gql`
   }
 `;
 
+// export const App = () => {
+//   const { loading, data, error } = useQuery(APP_QUERY);
+//   if (loading) {
+//     return <div>Loading!</div>;
+//   }
+//   if (error) {
+//     return <div>Error: {error}</div>;
+//   }
+//   return <div>{data.message}</div>;
+// };
 
 export const App = () => {
-
-  const { loading, data, error } = useQuery(APP_QUERY);
-
-  if (loading) {
-    return <div>Loading!</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
-  }
-
-  return <div>{data.message}</div>;
+  return <Query query={APP_QUERY}>
+    {({ loading, error, data}) => {
+      if (loading) {
+        return <div>Loading!</div>;
+      }
+      if (error) {
+        return <div>Error: {error}</div>;
+      }
+      return <div>{data.message}!!!</div>;
+    }}
+  </Query>;
 };
