126bc24fdf3056ea54ee222e8423afba818c2797
Eric Greene <eric@t4d.io>
2019-10-31 11:54:05 -0700

    add code demos

--- apollo-graphql-app-color-tool/graphql-server/resolvers.js
+++ apollo-graphql-app-color-tool/graphql-server/resolvers.js
@@ -3,5 +3,9 @@ import fetch from 'node-fetch';
 export const resolvers = {
   Query: {
     message: () => 'Hello World!',
+    colors: () => {
+      return fetch('http://localhost:3040/colors')
+        .then(res => res.json());
+    }
   },
 };
--- apollo-graphql-app-color-tool/graphql-server/schema.js
+++ apollo-graphql-app-color-tool/graphql-server/schema.js
@@ -1,5 +1,12 @@
 export const typeDefs = `
   type Query {
     message: String
+    colors: [Color]
+  }
+
+  type Color {
+    id: ID
+    name: String
+    hexcode: String
   }
 `;
